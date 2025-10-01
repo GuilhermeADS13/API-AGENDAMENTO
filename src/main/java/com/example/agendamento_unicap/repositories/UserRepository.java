b25ddf8d47f13b79ff0abdd70591ac0e308ba07d
MatheusOliveir4 <matheusfloliveira@gmail.com>
2025-10-01 19:49:43 -0300

    created UserRepository

---
+++ src/main/java/com/example/agendamento_unicap/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.agendamento_unicap.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.agendamento_unicap.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
